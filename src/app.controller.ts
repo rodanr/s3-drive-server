ce663a2e2f29dd88c99ee729b167a690dfa23a36
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-11-17 14:42:27 +0545

    consume s3 service in app module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { S3Service } from './aws/s3/s3.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly s3Service: S3Service,
+  ) {}
 
   @Get('/health')
   getHealth(): string {
     return this.appService.getHealth();
+  }
+
+  @Get('/upload-presigned-url')
+  getUploadPresignedURL() {
+    return this.s3Service.getUploadPresignedURL();
   }
 }
