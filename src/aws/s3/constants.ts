119b66f3ad5100241c94de286a59a4b5a138c7a6
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-11-17 19:32:14 +0545

    remove filename parser and add display size parser

--- src/aws/s3/constants.ts
+++ src/aws/s3/constants.ts
@@ -1,3 +1,4 @@
 export const FILE_SIZE = {
   ONE_GB: 1073741824,
+  ONE_MB: 1048576,
 };
--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,3 +1,9 @@
-export const s3ObjectKeyToFileName = (key: string): string => {
-  return key.split('/')[2];
+import { FILE_SIZE } from 'src/aws/s3/constants';
+
+export const getDisplaySizeFromBytes = (sizeInBytes: number): string => {
+  if (sizeInBytes >= FILE_SIZE.ONE_GB) {
+    return `${sizeInBytes / FILE_SIZE.ONE_GB} GB`;
+  } else {
+    return `${sizeInBytes / FILE_SIZE.ONE_MB} MB`;
+  }
 };
