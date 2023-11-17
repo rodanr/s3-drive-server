fc3e6de69b9670090081fcf93f1a4141a32658be
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-11-17 19:37:19 +0545

    limit display file size to 2 decimal points

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -2,8 +2,8 @@ import { FILE_SIZE_IN_BYTES } from 'src/aws/s3/constants';
 
 export const getDisplaySizeFromBytes = (sizeInBytes: number): string => {
   if (sizeInBytes >= FILE_SIZE_IN_BYTES.ONE_GB) {
-    return `${sizeInBytes / FILE_SIZE_IN_BYTES.ONE_GB} GB`;
+    return `${(sizeInBytes / FILE_SIZE_IN_BYTES.ONE_GB).toFixed(2)} GB`;
   } else {
-    return `${sizeInBytes / FILE_SIZE_IN_BYTES.ONE_MB} MB`;
+    return `${(sizeInBytes / FILE_SIZE_IN_BYTES.ONE_MB).toFixed(2)} MB`;
   }
 };
