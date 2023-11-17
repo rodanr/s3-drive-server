7362e794cc10e3031f59618b12f47608c8f184d6
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-11-17 14:43:18 +0545

    fix typo

--- src/aws/s3/s3.service.ts
+++ src/aws/s3/s3.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class S3Service {
   getUploadPresignedURL(): string {
-    return process.env?.AWS_S3_BUCKET_NAM;
+    return process.env?.AWS_S3_BUCKET_NAME;
   }
 }
