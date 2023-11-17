eb73530d5f9ddb5d1476f0aedd16ff197c8fbedf
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-11-17 14:35:49 +0545

    dummy implement s3 service

--- src/aws/s3/s3.service.ts
+++ src/aws/s3/s3.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class S3Service {}
+export class S3Service {
+  getUploadPresignedURL(): string {
+    return process.env?.AWS_S3_BUCKET_NAM;
+  }
+}
