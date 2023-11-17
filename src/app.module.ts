5694e356b5cb1806ee9e3db4c5f4fdb58cad4a1e
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-11-17 14:33:36 +0545

    enable env support

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { S3Service } from './aws/s3/s3.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, S3Service],
 })
